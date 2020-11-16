acbf977a6372fa5557218e1a08087194648929a2
mamathaguntu <mamathaguntu@gmail.com>
2020-11-15 19:42:12 -0800

    shell

--- shell_start.py
+++ shell_start.py
@@ -1,6 +1,8 @@
 import os
 from os import path
 import shutil
+from shutil import make_archive
+from zipfile import ZipFile
 
 def main():
     #duplicate an existing file
@@ -8,9 +10,25 @@ def main():
     if (path.exists(filename)):
         src = path.realpath(filename) 
         dest = src + ".bak"
-        shutil.copy(src,dest) # copies only the contents of the file
+        # shutil.copy(src,dest) # copies only the contents of the file
 
-        # copy the metadata(modification time , permissions, and other info) along with the file
-        shutil.copystat(src,dest)
+        # # copy the metadata(modification time , permissions, and other info) along with the file
+        # shutil.copystat(src,dest)
+
+        # rename the original file 
+        # os.rename("newfile.txt",filename)
+
+     
+        # root_dir, tail = path.split(src)
+
+        # archive entire directory
+        # shutil.make_archive("archive", "zip", root_dir)
+        
+        # create custom zip file with required files
+        with ZipFile("testzip.zip", "w") as newzip:
+            newzip.write("textfile.txt")
+            newzip.write("textfile.txt.bak")
+
+        
 if __name__ == "__main__":
     main()
